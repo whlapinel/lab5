8e6cb0f5dadf2e91d64504a90985a61d884f163b
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-10-06 14:52:26 -0400

    completed part 1

--- js/part1.js
+++ js/part1.js
@@ -5,17 +5,33 @@
  * @param {array} cards - an array of cards
  * @returns {string} - message 
  */
+
+
 function countCards(cards) {
-   
-        
+    let count = 0;
+    let message = '';
+    for (let card of cards) {
+        if (card >= 2 && card <= 6) {
+            count += 1;
+        } 
+        else if (card === 10 || card === 'J' || card === 'Q' || card === 'K' || card === 'A') {
+            count -= 1;
+        }
+    }
+    if (count > 0) {
+        message = 'Bet';
+    } else {
+        message = 'Hold';
+    }
+    return `${count} ${message}`
 }
-//uncomment following test code after implementing the function
-// console.log(countCards([2, 3, 7, 'K', 'A']));
-// console.log(countCards([2, 3, 4, 5, 6]));
-// console.log(countCards([7, 8, 9]));
-// console.log(countCards([10, 'J', 'Q', 'K', 'A']));
-// console.log(countCards([3, 7, 'Q', 8, 'A']));
-// console.log(countCards([2, 2, 10]));
-// console.log(countCards([2, 9, 'J', 2, 7]));
-// console.log(countCards([3, 2, 'A', 10, 'K']));
+
+console.log(countCards([2, 3, 7, 'K', 'A']));//0 Hold
+console.log(countCards([2, 3, 4, 5, 6]));//5 Bet
+console.log(countCards([7, 8, 9]));//0 Hold
+console.log(countCards([10, 'J', 'Q', 'K', 'A']));//-5 Hold
+console.log(countCards([3, 7, 'Q', 8, 'A']));//-1 Hold
+console.log(countCards([2, 2, 10]));//1 Bet
+console.log(countCards([2, 9, 'J', 2, 7]));//1 Bet
+console.log(countCards([3, 2, 'A', 10, 'K'])); //-1 Hold
 
