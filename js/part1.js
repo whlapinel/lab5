0bfda54a640330619dc260da571e86e56dbb32ac
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-10-06 12:04:54 -0400

    add starter files

---
+++ js/part1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * Returns a string of the count and Bet or Hold
+ * @param {array} cards - an array of cards
+ * @returns {string} - message 
+ */
+function countCards(cards) {
+   
+        
+}
+//uncomment following test code after implementing the function
+// console.log(countCards([2, 3, 7, 'K', 'A']));
+// console.log(countCards([2, 3, 4, 5, 6]));
+// console.log(countCards([7, 8, 9]));
+// console.log(countCards([10, 'J', 'Q', 'K', 'A']));
+// console.log(countCards([3, 7, 'Q', 8, 'A']));
+// console.log(countCards([2, 2, 10]));
+// console.log(countCards([2, 9, 'J', 2, 7]));
+// console.log(countCards([3, 2, 'A', 10, 'K']));
+
